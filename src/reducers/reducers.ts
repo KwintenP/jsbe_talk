5cc3d18823658f085283065201f8981f098685a5
kwintenp <kwinten.pisman@gmail.com>
2016-08-16 18:21:17 +0200

    Formatting.

--- src/reducers/reducers.ts
+++ src/reducers/reducers.ts
@@ -1,17 +1,27 @@
 import {Tweet} from "../entities/tweet.entity";
 import {Action} from "@ngrx/store";
 import {
-    TOGGLE_SIDEBAR, TOGGLE_TOPBAR, ADD_TWEET, SET_TWEETS, REMOVE_TWEET, TOGGLE_STAR_TWEET,
-    UPDATE_TWEET, TWEET_RETWEETED, toggleStarTweet, TWEET_UN_RETWEETED, TWEET_LIKED, TWEET_UN_LIKED
+    TOGGLE_SIDEBAR,
+    TOGGLE_TOPBAR,
+    ADD_TWEET,
+    SET_TWEETS,
+    REMOVE_TWEET,
+    TOGGLE_STAR_TWEET,
+    UPDATE_TWEET,
+    TWEET_RETWEETED,
+    TWEET_UN_RETWEETED,
+    TWEET_LIKED,
+    TWEET_UN_LIKED
 } from "../actions";
 import {Object} from "es6-shim";
+
 export const rootReducer = {
     sidebarCollapsed: sidebarReducer,
     topbarCollapsed: topbarReducer,
     tweets: tweetsReducer
 }
 
-export function sidebarReducer(state:boolean = false, action:Action):boolean {
+export function sidebarReducer(state: boolean = false, action: Action): boolean {
     switch (action.type) {
         case TOGGLE_SIDEBAR:
             return !state;
@@ -19,7 +29,7 @@ export function sidebarReducer(state:boolean = false, action:Action):boolean {
             return state;
     }
 }
-export function topbarReducer(state:boolean = false, action:Action):boolean {
+export function topbarReducer(state: boolean = false, action: Action): boolean {
     switch (action.type) {
         case TOGGLE_TOPBAR:
             return !state;
@@ -28,8 +38,8 @@ export function topbarReducer(state:boolean = false, action:Action):boolean {
     }
 }
 
-export function tweetsReducer(state:Array<Tweet> = [], action:Action):Array<Tweet> {
-    let id:number, tweet:Tweet, tweets: Array<Tweet>;
+export function tweetsReducer(state: Array<Tweet> = [], action: Action): Array<Tweet> {
+    let id: number, tweet: Tweet, tweets: Array<Tweet>;
     switch (action.type) {
         case ADD_TWEET:
             ({tweet} = action.payload);
@@ -42,14 +52,17 @@ export function tweetsReducer(state:Array<Tweet> = [], action:Action):Array<Twee
             return [...tweets];
         case UPDATE_TWEET:
             ({id, tweet} = action.payload);
-            return state.map(tweet => tweet.id == id? tweet: tweet);
+            return state.map(tweet => tweet.id == id ? tweet : tweet);
         case TWEET_UN_LIKED:
         case TWEET_LIKED:
         case TWEET_UN_RETWEETED:
         case TWEET_RETWEETED:
         case TOGGLE_STAR_TWEET:
             ({id} = action.payload);
-            return state.map(tweet => tweet.id == id? tweetReducer(tweet, {type: action.type, payload: {tweet}}): tweet);
+            return state.map(tweet => tweet.id == id ? tweetReducer(tweet, {
+                type: action.type,
+                payload: {tweet}
+            }) : tweet);
         default:
             return state;
     }
